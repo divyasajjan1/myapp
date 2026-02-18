2d0091091cdd1bc23cccdabc42adeb00066274d0
“Divya <divyasajjan@yahoo.co.in>
2026-02-18 11:34:36 -0500

    corrected settings and requirements for static

--- vetl/settings.py
+++ vetl/settings.py
@@ -30,12 +30,15 @@ ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://vetl-czhygkckfbgwcwh9.canadacentral-01.azurewebsites.net']
 CORS_ALLOWED_ORIGINS = ['https://vetl-czhygkckfbgwcwh9.canadacentral-01.azurewebsites.net']
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Application definition
@@ -126,7 +129,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
